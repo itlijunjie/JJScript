869ccca55032b846a9166a6aadaa927ca30c7abf
jj <jj.li@wanmei.com>
2015-11-23 14:37:28 +0800

    modify 修改debug配置下的bug
    修改debug配置下的bug
    修改README

--- README.md
+++ README.md
@@ -3,5 +3,23 @@
 ##pugongying.py（上传ipa到蒲公英的脚本）
     来源：https://github.com/answer-huang/pugongying/blob/master/pugongying.py
 ##releaseionic.sh（ionic打包发布到fir脚本）
+<pre>
+需要在./platforms/ios/cordova下的相应配置文件加上配置
+build-debug.xcconfig
+CODE_SIGN_IDENTITY = iPhone Developer
+CODE_SIGN_IDENTITY[sdk=iphoneos*] = iPhone Developer
+PROVISIONING_PROFILE = 5818b1c5
+ONLY_ACTIVE_ARCH = NO
+
+build-release.xcconfig
+CODE_SIGN_IDENTITY = iPhone Developer
+CODE_SIGN_IDENTITY[sdk=iphoneos*] = iPhone Developer
+PROVISIONING_PROFILE = 5818b1c5
+
+根据自己的实际情况做配置
+</pre>
 ##releasefir.sh（fir打包发布到fir脚本）
+<pre>
+使用debug配置发包的时候加上ONLY_ACTIVE_ARCH=NO
+</pre>
 ##outck.sh（根据cer和p12生成推送证书）
--- releasefir.sh
+++ releasefir.sh
@@ -6,13 +6,22 @@ out_path=`date "+%Y-%m-%d-%H-%M-%S"`
 
 if [ $# -eq 0 ]
 then
-fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c 发布新版本 CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead-4c-995e-1cb92ff13ece -o ./fir_build_ipa/$out_path
+fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c 发布新版本 CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead -o ./fir_build_ipa/$out_path
+
+
+#使用debug配置打包发布的时候别忘了加ONLY_ACTIVE_ARCH=NO，不知道为什么请自行补脑
+#fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c 发布新版本 CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead ONLY_ACTIVE_ARCH=NO -o ./fir_build_ipa/$out_path
+
+
 #    exit                     ##不存在退出
 else
 a=$*
 echo $a
-fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c $a CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead-4c-995e-1cb92ff13ece -o ./fir_build_ipa/$out_path
 
+fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c $a CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead -o ./fir_build_ipa/$out_path
+
+#使用debug配置打包发布的时候别忘了加ONLY_ACTIVE_ARCH=NO，不知道为什么请自行补脑
+fir b ./ -w -S PerfectDay -p -T f0bab9e48ac73ad5edeff7713b0b6b60 -C Release -c $a CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE=85b1de43-eead ONLY_ACTIVE_ARCH=NO -o ./fir_build_ipa/$out_path
 #    for a in $*             ##存在话，取参数放入a中循环
 #    do
 #        b=`echo $a|tr [a-z] [A-Z]`    ###将a改成大写
